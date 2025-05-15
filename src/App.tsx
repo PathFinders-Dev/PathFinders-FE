fc5db92288cbc85a5c42286be9da24f36b63d14a
jhyunwoo <jhyunwoo0228@gmail.com>
2025-05-15 18:41:13 +0900

    fix error finally

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import useWebHooks from "./hooks/useWebSocket";
 
 function App() {
   useWebHooks({
-    url: "ws://localhost:3000/ws",
+    url: "wss://api.wildfire.moveto.kr/ws",
     onOpen: () => {
       console.log("소켓 연결 성공");
     },
